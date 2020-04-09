1f355b1bb68be910d192c62111f2d7ad27d9703f
Steve Bottos <32916895+stevebottos@users.noreply.github.com>
2020-04-09 14:18:25 -0700

    Add files via upload

---
+++ Leetcode - Permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        if len(nums) == 0:
+            return []
+        
+        if len(nums) == 1:
+            return [nums]
+        
+        n = len(nums)
+        perm = []
+        
+        for i in range(n):
+            obj_at_i = nums[i]
+            other_elements_in_list = nums[:i] + nums[i+1:]
+            
+            print(other_elements_in_list)
+            for partial_permutation in Solution.permute(self, other_elements_in_list):
+                perm.append([obj_at_i] + partial_permutation)
+                
+        return perm
