9bf01cb33e8fc534569a5613cb0571978d861160
Steve Bottos <32916895+stevebottos@users.noreply.github.com>
2020-04-27 10:21:50 -0700

    Adding comments for clarity

--- Leetcode - Diagonal Traverse II.py
+++ Leetcode - Diagonal Traverse II.py
@@ -1,7 +1,11 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        
+        # We'll start with initializing a hash table to store the values in terms of their diagonal
         hash_table = {}
         
+        # The diagonal which an element belongs to can be obtained as "array_number + index_number"
+        # We'll compute the diagonal and just add it to the hash table where it belongs
         for arr_num, arr in enumerate(nums):
             for idx, element in enumerate(arr):
                 if arr_num + idx not in hash_table:
@@ -9,6 +13,7 @@ class Solution:
                 else:
                     hash_table[arr_num + idx] = [element] + hash_table[arr_num + idx]
         
+        # Finally just run through the hash table and add each key's list to a final list that we will return
         nums_list = []
         for key in hash_table:
             nums_list += hash_table[key]
