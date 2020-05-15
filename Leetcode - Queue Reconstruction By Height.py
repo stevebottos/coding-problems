dd36157be19b3a47f7367e7dead35cb7a0e67ac6
Steve Bottos <32916895+stevebottos@users.noreply.github.com>
2020-05-15 13:25:11 -0700

    Did a bit of work on this, fails some test cases

--- Leetcode - Queue Reconstruction By Height.py
+++ Leetcode - Queue Reconstruction By Height.py
@@ -1,44 +1,44 @@
+# Test case failed on: [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        # The sorting is important
         people = sorted(people)
-        max_val = people[-1][0]
-        print(max_val)
         
-        # Zero members simply need to be sorted based on their first values
+        # Some additional starting values 
         zero_members = [i for i in people if i[1] == 0]
-        # Keep this guy sorted too
         leftovers = [i for i in people if i[1] != 0]
+        hash_table_indices = set([i[0] for i in people])
         
-        
-        # Now let's create a hash table that keeps track of how many of each value we have in the current stack
+        # Now let's create a hash table that keeps track of how many "greater than" values we have
         ht = {}
-        for i in range(1,max_val+1):
-            ht[i] = 0
-        
+        for key in hash_table_indices:
+            ht[key] = 0
+    
+        del hash_table_indices
+    
+        # Now let's start with the zero members since they're simply inserted into the table in order
         for el in zero_members:
-            ht[el[0]] += 1
+            n = el[0]
+            print(n)
+            for key in ht:
+                if n >= key:
+                    ht[key] += 1
+                
         
+        # Now we can just treat this as a stack
         stack = zero_members
         # Next let's loop through the list that we have an insert when applicable
         while leftovers:
             for el in leftovers:
                 if ht[el[0]] >= el[1]:
-                    print(el)
+                    stack.append(el)
+                    leftovers.remove(el)
+                    n = el[0]
+                    break
                     
-            break
+            for key in ht:
+                if n >= key:
+                    ht[key] += 1
+            
         
-        
-        
-#         
-        
-#         # Create a hash table with the count of each thing:
-#         ht = {}
-        
-#         for el in zero_members:
-#             key = el[0]
-#             if key not in ht:
-#                 ht[key] = 1
-#             else:
-#                 ht[key] += 1
-        
-#         # Next, insert where applicaple
+        return stack
